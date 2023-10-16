a29f49ac7a3f97e6abf7120653d751ed7458ca85
Kayque-Silva <kayque563@gmail.com>
2023-10-16 15:21:37 -0300

    Ultimo exercício módulo javaScript

---
+++ javascript/tryecatch.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+try {
+    let num1 = parseFloat(prompt('Digite um número: '));
+    let num2 = parseFloat(prompt('Digite outro numero: '));
+    resultado = num1 / num2;
+    console.log(`O resultado da divisão entre os números é: ${resultado}`)
+} catch (error) {
+    console.log('Erro:', error.message );
+} finally {
+    console.log('Bloco "finally" sempre será executado.');
+}
